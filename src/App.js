cdfbe3d791b20fad093f88cbbc4b039f40eb9f24
Thailson Betim <thailson.betim.tb@gmail.com>
2021-10-05 16:40:43 -0300

    Feat: filtro finalizado

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import Content from './Components/Content';
-import Header from './Components/Header';
-import { autoLogin } from './store/login';
 import './App.css';
+import Filter from './Filter';
+import Products from './Products';
 
 function App() {
-  const dispatch = useDispatch();
-
-  React.useEffect(() => {
-    dispatch(autoLogin());
-  }, [dispatch]);
-
   return (
-    <div className="container">
-      <Header />
-      <Content />
+    <div>
+      <Filter />
+      <Products />
     </div>
   );
 }
